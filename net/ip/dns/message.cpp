9661978d8f9fc6971f3d02e2aeda56c0a94d279c
Guido Reina <guido.reina@yahoo.com>
2019-12-08 14:40:31 +0100

    Added class net::ip::dns::message

---
+++ net/ip/dns/message.cpp
@@ -0,0 +1,275 @@
+#include <string.h>
+#include "net/ip/dns/message.h"
+
+bool net::ip::dns::message::parse(const void* buf, size_t len)
+{
+  // Save parameters.
+  _M_buf = static_cast<const uint8_t*>(buf);
+  _M_len = len;
+
+  // If the DNS message should be processed...
+  if ((_M_len >= header_len)           && // The message is not too short.
+      (_M_len <= max_len)              && // The message is not too long.
+      (((_M_buf[2] >> 3) & 0x0f) <= 2) && // 0 <= OPCODE <= 2
+      ((_M_buf[2] & 0x02) == 0)        && // The message was not truncated.
+      ((_M_buf[3] & 0x0f) == 0)) {        // RCODE = 0
+    const uint16_t
+      qdcount = (static_cast<uint16_t>(_M_buf[4]) << 8) | _M_buf[5];
+
+    // If there are questions...
+    if (qdcount > 0) {
+      _M_off = header_len;
+
+      // If the QNAME is valid and the QTYPE and QCLASS fit...
+      if ((parse_domain_name()) && (_M_off + 4 <= _M_len)) {
+        // If the QCLASS is 1 (IN [Internet])...
+        if (((static_cast<uint16_t>(_M_buf[_M_off + 2]) << 8) |
+             _M_buf[_M_off + 3]) == 1) {
+          // Get QTYPE.
+          const uint16_t qtype = (static_cast<uint16_t>(_M_buf[_M_off]) << 8) |
+                                 _M_buf[_M_off + 1];
+
+          if (qtype <= 255) {
+            // Save query type.
+            _M_qtype = static_cast<uint8_t>(qtype);
+
+            // No responses so far.
+            _M_nresponses = 0;
+
+            // Query?
+            if ((_M_buf[2] & 0x80) == 0) {
+                return true;
+            } else {
+              const uint16_t ancount = (static_cast<uint16_t>(_M_buf[6]) << 8) |
+                                       _M_buf[7];
+
+              // If there are answers...
+              if (ancount > 0) {
+                // Skip QTYPE and QCLASS.
+                _M_off += 4;
+
+                // Skip following questions (if any).
+                for (size_t i = 2; i <= qdcount; i++) {
+                  if (!skip_question()) {
+                    return false;
+                  }
+                }
+
+                // Process answers.
+                for (size_t i = 1; i <= ancount; i++) {
+                  if ((skip_domain_name()) && (_M_off + 10 <= _M_len)) {
+                    const uint16_t rdlength =
+                      (static_cast<uint16_t>(_M_buf[_M_off + 8]) << 8) |
+                      _M_buf[_M_off + 9];
+
+                    const size_t next = _M_off + 10 + rdlength;
+                    if (next <= _M_len) {
+                      // If the CLASS is 1 (IN [Internet])...
+                      if (((static_cast<uint16_t>(_M_buf[_M_off + 2]) << 8) |
+                           _M_buf[_M_off + 3]) == 1) {
+                        // Check type.
+                        switch ((static_cast<uint16_t>(_M_buf[_M_off]) << 8) |
+                                _M_buf[_M_off + 1]) {
+                          case 1: // A (host address [IPv4]).
+                            if (rdlength == 4) {
+                              _M_responses[_M_nresponses].family = AF_INET;
+
+                              memcpy(_M_responses[_M_nresponses].addr,
+                                     _M_buf + _M_off + 10,
+                                     4);
+
+                              if (++_M_nresponses == max_responses) {
+                                return true;
+                              }
+                            } else {
+                              return false;
+                            }
+
+                            break;
+                          case 28: // AAAA (IPv6).
+                            if (rdlength == 16) {
+                              _M_responses[_M_nresponses].family = AF_INET6;
+
+                              memcpy(_M_responses[_M_nresponses].addr,
+                                     _M_buf + _M_off + 10,
+                                     16);
+
+                              if (++_M_nresponses == max_responses) {
+                                return true;
+                              }
+                            } else {
+                              return false;
+                            }
+
+                            break;
+                        }
+                      }
+
+                      _M_off = next;
+                    } else {
+                      return false;
+                    }
+                  } else {
+                    return false;
+                  }
+                }
+
+                return (_M_nresponses > 0);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+bool net::ip::dns::message::parse_domain_name()
+{
+  size_t len = 0;
+  size_t npointers = 0;
+
+  // Work with a copy of the offset.
+  size_t off = _M_off;
+
+  while (off < _M_len) {
+    switch (_M_buf[off] & 0xc0) {
+      case 0: // Label.
+        // If not the null label...
+        if (_M_buf[off] > 0) {
+          const size_t next = off + 1 + _M_buf[off];
+          if ((next < _M_len) &&
+              (len + 1 + _M_buf[off] <= domain_name_max_len)) {
+            // If not the first label...
+            if (len > 0) {
+              _M_domain[len++] = '.';
+            }
+
+            // Copy label.
+            memcpy(_M_domain + len, _M_buf + off + 1, _M_buf[off]);
+
+            len += _M_buf[off];
+
+            off = next;
+          } else {
+            return false;
+          }
+        } else {
+          // Null label.
+
+          // If not the root domain name...
+          if ((_M_domainlen = static_cast<uint8_t>(len)) > 0) {
+            if (npointers == 0) {
+              _M_off = off + 1;
+            }
+
+            _M_domain[len] = 0;
+
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        break;
+      case 0xc0: // Pointer.
+        if ((++npointers <= max_pointers) && (off + 1 < _M_len)) {
+          // Compute pointer offset.
+          const size_t
+            ptroff = (static_cast<uint16_t>(_M_buf[off] & 0x3f) << 8) |
+                     _M_buf[off + 1];
+
+          // Valid offset?
+          if ((ptroff >= header_len) && (ptroff < _M_len)) {
+            // First pointer?
+            if (npointers == 1) {
+              _M_off = off + 2;
+            }
+
+            off = ptroff;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+
+        break;
+      default:
+        return false;
+    }
+  }
+
+  return false;
+}
+
+bool net::ip::dns::message::skip_domain_name()
+{
+  size_t len = 0;
+  size_t npointers = 0;
+
+  // Work with a copy of the offset.
+  size_t off = _M_off;
+
+  while (off < _M_len) {
+    switch (_M_buf[off] & 0xc0) {
+      case 0: // Label.
+        // If not the null label...
+        if (_M_buf[off] > 0) {
+          const size_t next = off + 1 + _M_buf[off];
+          if ((next < _M_len) &&
+              (len + 1 + _M_buf[off] <= domain_name_max_len)) {
+            // If not the first label...
+            if (len > 0) {
+              len += (1 + _M_buf[off]);
+            } else {
+              len += _M_buf[off];
+            }
+
+            off = next;
+          } else {
+            return false;
+          }
+        } else {
+          // Null label.
+
+          if (npointers == 0) {
+            _M_off = off + 1;
+          }
+
+          return true;
+        }
+
+        break;
+      case 0xc0: // Pointer.
+        if ((++npointers <= max_pointers) && (off + 1 < _M_len)) {
+          // Compute pointer offset.
+          const size_t
+            ptroff = (static_cast<uint16_t>(_M_buf[off] & 0x3f) << 8) |
+                     _M_buf[off + 1];
+
+          // Valid offset?
+          if ((ptroff >= header_len) && (ptroff < _M_len)) {
+            // First pointer?
+            if (npointers == 1) {
+              _M_off = off + 2;
+            }
+
+            off = ptroff;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+
+        break;
+      default:
+        return false;
+    }
+  }
+
+  return false;
+}
---
+++ net/ip/dns/message.h
@@ -0,0 +1,133 @@
+#ifndef NET_IP_DNS_MESSAGE_H
+#define NET_IP_DNS_MESSAGE_H
+
+#include <stdint.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include "net/ip/limits.h"
+
+namespace net {
+  namespace ip {
+    namespace dns {
+      // DNS port in network byte order.
+      static constexpr const uint16_t port = static_cast<uint16_t>(53) << 8;
+
+      // DNS message.
+      class message {
+        public:
+          // Maximum number of responses.
+          static constexpr const size_t max_responses = 32;
+
+          // Constructor.
+          message() = default;
+
+          // Destructor.
+          ~message() = default;
+
+          // Parse DNS message.
+          bool parse(const void* buf, size_t len);
+
+          // Get query type.
+          uint8_t qtype() const;
+
+          // Get domain.
+          const char* domain() const;
+          const char* domain(size_t& len) const;
+
+          // Get number of responses.
+          size_t number_responses() const;
+
+          // DNS response.
+          struct response {
+            int family; // AF_INET or AF_INET6.
+            uint8_t addr[sizeof(struct in6_addr)];
+          };
+
+          // Get response.
+          const struct response* response(size_t idx) const;
+
+        private:
+          // Maximum length of a DNS message.
+          static constexpr const size_t max_len = 512;
+
+          // Length of the DNS header.
+          static constexpr const size_t header_len = 12;
+
+          // Maximum number of DNS pointers.
+          static constexpr const size_t max_pointers = 64;
+
+          // Message buffer.
+          const uint8_t* _M_buf;
+
+          // Message length.
+          size_t _M_len;
+
+          // Current offset.
+          size_t _M_off;
+
+          // Query type.
+          uint8_t _M_qtype = 0;
+
+          // Domain length.
+          uint8_t _M_domainlen = 0;
+
+          // Domain.
+          char _M_domain[domain_name_max_len + 1] = {0};
+
+          // # of DNS responses.
+          uint8_t _M_nresponses = 0;
+
+          // DNS responses.
+          struct response _M_responses[max_responses];
+
+          // Skip question.
+          bool skip_question();
+
+          // Parse domain-name.
+          bool parse_domain_name();
+
+          // Skip domain-name.
+          bool skip_domain_name();
+      };
+
+      inline uint8_t message::qtype() const
+      {
+        return _M_qtype;
+      }
+
+      inline const char* message::domain() const
+      {
+        return _M_domain;
+      }
+
+      inline const char* message::domain(size_t& len) const
+      {
+        len = _M_domainlen;
+        return _M_domain;
+      }
+
+      inline size_t message::number_responses() const
+      {
+        return _M_nresponses;
+      }
+
+      inline const struct message::response* message::response(size_t idx) const
+      {
+        return (idx < _M_nresponses) ? &_M_responses[idx] : nullptr;
+      }
+
+      inline bool message::skip_question()
+      {
+        if ((skip_domain_name()) && (_M_off + 4 <= _M_len)) {
+          _M_off += 4;
+
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+}
+
+#endif // NET_IP_DNS_MESSAGE_H
